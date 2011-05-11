e6e8d5713572670ea261a535435b0093b450947f
Rodrigo Bonifácio <rodrigobonifacio@Rodrigo-Bonifacios-MacBook.local>
2011-05-11 19:24:17 -0300

    Compilacao deve referenciar src/Main.hs, em vez de main/VMGui.hs

--- main/compile.sh
+++ main/compile.sh
@@ -1,2 +1,2 @@
-ghc -threaded --make -odir=../dist -isrc/:../feature-modeling/src/:../configuration-knowledge/src/:../core/src/:../uc-model/src/:../req-model/src:../transormations/src/:../component-model/src:../bpmn/src src/VMGui.lhs 
+ghc -threaded --make -odir=../dist -isrc/:../feature-modeling/src/:../configuration-knowledge/src/:../core/src/:../uc-model/src/:../req-model/src:../transormations/src/:../component-model/src:../bpmn/src src/Main.hs 
 
